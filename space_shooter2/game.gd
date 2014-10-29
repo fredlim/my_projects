016ac02b14856b403708d78afff7a484260d4a42
fredlim <hungwai.lim@gmail.com>
2014-10-29 17:14:23 +0800

    Update mouse control player movement
    First approach of moving the player using mouse
    Add a projectile with animation

--- space_shooter2/game.gd
+++ space_shooter2/game.gd
@@ -1,33 +1,19 @@
 
 extends Node2D
 
-var last_player_pos
-var player_pos
-var moving = "center"
-var move_state
+var nPlayer
 
 func _ready():
+	nPlayer = get_node("Player")
+	nPlayer.set_animation("center")
 	set_process(true)
 	set_process_input(true)
-	get_node("Player").set_animation("center")
 
-func _process(delta):
-	player_pos = get_node("Player").get_pos()
-	#if (last_player_pos == player_pos):
-	#	moving = "center"
-	print(moving)
-	#get_node("Player").set_animation(moving)
-	#last_player_pos = player_pos
+
+func _process(dt):
+	pass
 	
+
 func _input(ev):
-	var ev_pos = ev.pos
-
 	if (ev.type==InputEvent.MOUSE_MOTION):
-		get_node("Player").set_pos(ev_pos)
-		
-	if (ev_pos.x < player_pos.x):
-		moving = "left"
-	elif (ev_pos.x > player_pos.x):
-		moving = "right"
-
-	get_node("Player").set_animation(moving)
+		nPlayer.setterPos(ev.pos)
--- space_shooter2/player.gd
+++ space_shooter2/player.gd
@@ -1,14 +1,50 @@
 
 extends Sprite
 
+const CENTER = 0
+const LEFT = 1
+const RIGHT = 2
+const MOUSE_LAG = 0.32
+
+var move_state = CENTER #initial state
+var pos setget setterPos
+var mouse_move = false
+var mcount = MOUSE_LAG
 
 func _ready():
+	pos = Vector2(0,0)
 	set_process(true)
+
+
+func _process(dt):
+	var current_pos = self.get_pos()
+	if (mouse_move):
+		if (pos.x < current_pos.x):
+			if (move_state == CENTER or move_state == RIGHT):
+				move_state = LEFT
+				set_animation("left")
+		elif (pos.x > current_pos.x):
+			if (move_state == CENTER or move_state == LEFT):
+				move_state = RIGHT
+				set_animation("right")
+		
+		if (mcount < 0):
+			mouse_move = false
+			mcount = MOUSE_LAG
+		else:
+			mcount = mcount - 1*dt
+			
+	else:
+		if (move_state != CENTER):
+			move_state = CENTER
+			set_animation("center")
+	self.set_pos(pos)
+
+
+func setterPos(val):
+	pos = val
+	mouse_move = true
 	
 
-func _process(d):
-	pass
-		
-	
 func set_animation(a):
 	get_node("Animation").play(a,-1,1,false)
