92e0c9f6debf520035d16e228c3840dcdb853b27
fredlim <hungwai.lim@gmail.com>
2014-10-31 17:24:00 +0800

    Making projectile pool manager
    Not finish yet

--- space_shooter2/game.gd
+++ space_shooter2/game.gd
@@ -1,14 +1,19 @@
 
-extends Node2D
+extends Node
 
-var main_projectile = preload("res://main_projectile.res")
+#var main_projectile = preload("res://main_projectile.res")
 
+var canvas_node
+var mpmanager
 var nPlayer
 
+
 func _ready():
-	nPlayer = get_node("Player")
+	canvas_node = get_node("/root/Game/Canvas")
+	mpmanager = get_node("MainProjectile_manager")
+	nPlayer = get_node("Canvas/Player")
 	nPlayer.set_animation("center")
-	set_process(true)
+	#set_process(true)
 	set_process_input(true)
 
 
@@ -21,10 +26,13 @@ func _input(ev):
 		nPlayer.setterPos(ev.pos)
 		
 	if (ev.type==InputEvent.MOUSE_BUTTON):
-		if (Input.is_mouse_button_pressed(1)):
-			var main_projectile_instance = main_projectile.instance()
-			main_projectile_instance.set_name("projectile")
-			add_child(main_projectile_instance)
+		pass
+		#if (Input.is_mouse_button_pressed(1)):
+			# check projectile manager for available projectile node
+			#var mp = mpmanager.available()
 			
-			get_node("projectile").set_pos(ev.pos)
+			# get player position and pass position to projectile
+			#mp.set_pos(nPlayer.get_pos())
 			
+			# call projectile to show and run
+			#mp.show()
