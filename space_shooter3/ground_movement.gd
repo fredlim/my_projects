236a0889c6accc9f19279fff86cd0e0507d3cd55
hungwailim <hungwai.lim@gmail.com>
2015-01-21 00:38:48 +0800

    New test map

--- space_shooter3/ground_movement.gd
+++ space_shooter3/ground_movement.gd
@@ -1,12 +1,15 @@
 
 extends Position2D
 
+var map
 var scroll = 0
+var scrollspeed = 50
 
 func _ready():
+	map = preload("res://map1.scn")
 	set_process(true)
 
 func _process(delta):
-	scroll+=50*delta
+	scroll += 50 * delta
 	set_pos(Vector2(0, scroll))
 	
