4d84761fb21d8ad3e66aa5e808cca970e2490344
fredlim <hungwai.lim@gmail.com>
2015-01-22 10:49:06 +0800

    Fixed background flickering
    Change map scene origin to negative screen resolution to avoid popup
    effect when create a new instance

--- space_shooter3/map_scroll.gd
+++ space_shooter3/map_scroll.gd
@@ -12,10 +12,12 @@ func _ready():
 
 func _process(delta):
 	pos += scrollspeed * delta
-	set_pos(Vector2(0, pos))
+	
 	if (pos > SCREEN_H):
 		get_parent().add_map(pos - SCREEN_H)
 		queue_free()
+	else:
+		set_pos(Vector2(0, pos))
 
 
 func setpos(p):
